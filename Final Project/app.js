9d6094cccef80bce9b0cff4ab93a731f2f64689f
Eri <laurengoff@Laurens-MacBook-Pro.local>
2018-01-12 19:59:27 -0800

    lots of comments
    #BestComments #BestCommitMessage

--- Final Project/app.js
+++ Final Project/app.js
@@ -4,7 +4,7 @@
     window.requestAnimationFrame = requestAnimationFrame;
 })();
 
-
+//Canvas
 var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
     width = 500,
@@ -13,13 +13,17 @@ var canvas = document.getElementById("canvas"),
       x : width/2,
       y : height - 5,
       width : 5,
-      height : 5
-    };
+      height : 5,
+      speed: 3,
+      velX: 0,
+      velY: 0
+    },
+    keys = [];
  
 canvas.width = width;
 canvas.height = height;
  
-// draw a small red box, which will eventually become our player.
+// draw a small red box, which will eventually become our player. Will eventually replace with a sprite. Maybe. 
 function update(){
     // draw our player
     ctx.fillStyle = "red";
@@ -27,8 +31,23 @@ function update(){
     // run through the loop again
     requestAnimationFrame(update);
 }
-
+//this is important don't delete it you idiot 
 window.addEventListener("load", function(){
   update();
 });
+
+//yay keyboard events!! #BestComments #BestCoder
+document.body.addEventListener("keydown", function(e) {
+    keys[e.keyCode] = true;
+});
+ 
+document.body.addEventListener("keyup", function(e) {
+    keys[e.keyCode] = false;
+});
+
+
+
+
+
+
 
