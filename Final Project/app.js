02c0513fd16278ca1b67094b117361c2e4813bb0
Eri <laurengoff@Laurens-MacBook-Pro.local>
2018-01-29 14:50:07 -0800

    There is a bug somewhere

--- Final Project/app.js
+++ Final Project/app.js
@@ -1,93 +1,29 @@
-//because set interval is kinda lame
-(function() {
+(function () {
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     window.requestAnimationFrame = requestAnimationFrame;
 })();
- //eventually replace player dot with sprite?
+
 var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
     width = 500,
     height = 200,
     player = {
-      x : width/2,
-      y : height - 5,
-      width : 5,
-      height : 5,
-      speed: 3,
-      velX: 0,
-      velY: 0,
-      jumping: false
+        x: width / 2,
+        y: height - 15,
+        width: 5,
+        height: 5,
+        speed: 3,
+        velX: 0,
+        velY: 0,
+        jumping: false,
+        grounded: false
     },
     keys = [],
     friction = 0.8,
-    gravity = 0.2;
- 
-canvas.width = width;
-canvas.height = height;
- 
-function update(){
-  // check keys
-    if (keys[38] || keys[32]) {
-        // up arrow or space
-      if(!player.jumping){
-       player.jumping = true;
-       player.velY = -player.speed*2;
-      }
-    }
-    if (keys[39]) {
-        // right arrow
-        if (player.velX < player.speed) {             
-            player.velX++;         
-         }     
-    }     
-    if (keys[37]) {         
-        // left arrow         
-        if (player.velX > -player.speed) {
-            player.velX--;
-        }
-    }
- 
-    player.velX *= friction;
- 
-    player.velY += gravity;
- 
-    player.x += player.velX;
-    player.y += player.velY;
- 
-    if (player.x >= width-player.width) {
-        player.x = width-player.width;
-    } else if (player.x <= 0) {         
-        player.x = 0;     
-    }    
-  
-    if(player.y >= height-player.height){
-        player.y = height - player.height;
-        player.jumping = false;
-    }
- 
-  ctx.clearRect(0,0,width,height);
-  ctx.fillStyle = "red";
-  ctx.fillRect(player.x, player.y, player.width, player.height);
- 
-  requestAnimationFrame(update);
-}
- 
-document.body.addEventListener("keydown", function(e) {
-    keys[e.keyCode] = true;
-});
- 
-document.body.addEventListener("keyup", function(e) {
-    keys[e.keyCode] = false;
-});
- //this is important! Move it shift it don't delete it
-window.addEventListener("load",function(){
-    update();
-}); 
+    gravity = 0.3;
 
-//from this point we begin the code to deal with and draw collisions 
+var boxes = [];
 
-var boxes = []
- 
 // dimensions
 boxes.push({
     x: 0,
@@ -104,15 +40,144 @@ boxes.push({
 boxes.push({
     x: width - 10,
     y: 0,
-    width: 50,
+    width: 60,
     height: height
 });
 
+boxes.push({ //box on the right
+    x: 12,
+    y: 100,
+    width: 100,
+    height: 20
+});
+boxes.push({ //middle box
+    x: 180,
+    y: 40,
+    width: 100,
+    height: 30
+});
+boxes.push({
+    x: 120,
+    y:120, 
+    width: 100,
+    height: 40
+});
+boxes.push({
+    x: 270,
+    y: 150, 
+    width: 100,
+    height: 40
+});
+
+canvas.width = width;
+canvas.height = height;
+
+function update() {
+    // check keys
+    if (keys[38] || keys[32] || keys[87]) {
+        // up arrow or space
+        if (!player.jumping && player.grounded) {
+            player.jumping = true;
+            player.grounded = false;
+            player.velY = -player.speed * 2;
+        }
+    }
+    if (keys[39] || keys[68]) {
+        // right arrow
+        if (player.velX < player.speed) {
+            player.velX++;
+        }
+    }
+    if (keys[37] || keys[65]) {
+        // left arrow
+        if (player.velX > -player.speed) {
+            player.velX--;
+        }
+    }
+
+    player.velX *= friction;
+    player.velY += gravity;
+
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    
+    player.grounded = false;
+    for (var i = 0; i < boxes.length; i++) {
+        ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+        
+        var dir = colCheck(player, boxes[i]);
+
+        if (dir === "l" || dir === "r") {
+            player.velX = 0;
+            player.jumping = false;
+        } else if (dir === "b") {
+            player.grounded = true;
+            player.jumping = false;
+        } else if (dir === "t") {
+            player.velY *= -1;
+        }
+
+    }
+    
+    if(player.grounded){
+         player.velY = 0;
+    }
+    
+    player.x += player.velX;
+    player.y += player.velY;
+
+    ctx.fill();
+    ctx.fillStyle = "red";
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+
+    requestAnimationFrame(update);
+}
+
+function colCheck(shapeA, shapeB) {
+    // get the vectors to check against
+    var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+        vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+        // add the half widths and half heights of the objects
+        hWidths = (shapeA.width / 2) + (shapeB.width / 2),
+        hHeights = (shapeA.height / 2) + (shapeB.height / 2),
+        colDir = null;
+
+    // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
+    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+        // figures out on which side we are colliding (top, bottom, left, or right)
+        var oX = hWidths - Math.abs(vX),
+            oY = hHeights - Math.abs(vY);
+        if (oX >= oY) {
+            if (vY > 0) {
+                colDir = "t";
+                shapeA.y += oY;
+            } else {
+                colDir = "b";
+                shapeA.y -= oY;
+            }
+        } else {
+            if (vX > 0) {
+                colDir = "l";
+                shapeA.x += oX;
+            } else {
+                colDir = "r";
+                shapeA.x -= oX;
+            }
+        }
+    }
+    return colDir;
+}
+
+document.body.addEventListener("keydown", function (e) {
+    keys[e.keyCode] = true;
+});
+
+document.body.addEventListener("keyup", function (e) {
+    keys[e.keyCode] = false;
+});
 
 
-
-
-
-
-
-
+window.addEventListener("load", function () {
+    update();
+});
